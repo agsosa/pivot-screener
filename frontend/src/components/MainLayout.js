c1285ae29eba6d0612006bf3f5407e193a16ff34
aleegs <aleej.gs@gmail.com>
2021-02-17 10:25:41 -0300

    Calculator page

--- frontend/src/components/MainLayout.js
+++ frontend/src/components/MainLayout.js
@@ -4,29 +4,37 @@ import Sidebar from './Sidebar';
 import './MainLayout.css';
 import LayoutHeader from './LayoutHeader';
 import LayoutFooter from './LayoutFooter';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
+import CalculatorPage from './pages/CalculatorPage';
 
 const { Content } = Layout;
 
 export default function MainLayout(props) {
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <Sidebar />
-            <Layout className="site-layout">
-                <LayoutHeader />
+        <Router>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sidebar />
+                <Layout className="site-layout">
+                    <LayoutHeader />
 
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                    Bill is a cat.
-                    </div>
-                </Content>
+                    <Content style={{ margin: '0 16px' }}>
+                            <Switch>
+                                <Route exact path="/calculator" component={CalculatorPage} />
+                                <Route exact path="/cpr-screener/:market" component={null} />
+                                <Route exact path="/" component={null} />
+                            </Switch>
+                            
+                    </Content>
 
-                <LayoutFooter />
+                    <LayoutFooter />
 
+                </Layout>
             </Layout>
-        </Layout>
+        </Router>
     );
 }
