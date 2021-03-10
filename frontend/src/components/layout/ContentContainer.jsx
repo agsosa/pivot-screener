6ad1defe5bf8d6fdcbeaf5075f8bf822c2b4b485
aleegs <aleej.gs@gmail.com>
2021-03-10 02:36:06 -0300

    Updated ContentContainer

--- frontend/src/components/layout/ContentContainer.jsx
+++ frontend/src/components/layout/ContentContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import Breadcrumb from '../Breadcrumb';
+import './ContentContainer.css';
 
 const ContentContainer = ({ breadcrumbItems, children }) => (
-	<div className='site-layout-background' style={{ padding: 24, minHeight: 360, marginTop: 10, textAlign: 'center' }}>
-		<Breadcrumb items={breadcrumbItems} />
-		{children}
+	<div>
+		<div className='site-layout-background content-container'>
+			<Breadcrumb items={breadcrumbItems} />
+			{children}
+		</div>
 	</div>
 );
 
@@ -16,7 +19,7 @@ ContentContainer.defaultProps = {
 
 ContentContainer.propTypes = {
 	breadcrumbItems: PropTypes.arrayOf(PropTypes.string),
-	children: PropTypes.element,
+	children: PropTypes.node,
 };
 
 export default ContentContainer;
