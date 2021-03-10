a84847b71f5dbe330278b40768ce54de1f8b5db2
aleegs <aleej.gs@gmail.com>
2021-03-09 22:31:22 -0300

    Moved table filters menu actions to a separated file

---
+++ frontend/src/components/tables/FiltersMenu.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Space, Button, Badge } from 'antd';
+import './Table.css';
+import { PropTypes } from 'prop-types';
+import { capitalizeFirstLetter } from '../../lib/Helpers';
+import SocketStatus from '../SocketStatus';
+
+const FiltersMenu = ({ tickersCount, market, timeframe, onSaveFilters, onLoadFilters, onClearFilters }) => (
+	<div>
+		<Space className='space'>
+			<h1>
+				{capitalizeFirstLetter(market)} / {capitalizeFirstLetter(timeframe)}
+			</h1>
+			<Badge className='badge' count={tickersCount} />
+			<SocketStatus className='socket-status' />
+		</Space>
+		<p className='filter-hint'>You can filter, short and move any column. The data is updated automatically.</p>
+		<Space>
+			<Button onClick={onSaveFilters}>Save Filters</Button>
+			<Button onClick={onLoadFilters}>Load Saved Filters</Button>
+			<Button onClick={onClearFilters}>Clear Filters</Button>
+		</Space>
+	</div>
+);
+
+FiltersMenu.propTypes = {
+	tickersCount: PropTypes.number.isRequired,
+	market: PropTypes.string.isRequired,
+	timeframe: PropTypes.string.isRequired,
+	onSaveFilters: PropTypes.func.isRequired,
+	onLoadFilters: PropTypes.func.isRequired,
+	onClearFilters: PropTypes.func.isRequired,
+};
+
+export default FiltersMenu;
