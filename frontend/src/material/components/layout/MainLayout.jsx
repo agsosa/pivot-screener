e92da02e46f73ee4b8084e68d317ee689210fca5
aleegs <aleej.gs@gmail.com>
2021-04-26 01:48:35 -0300

    Create MainLayout component

---
+++ frontend/src/material/components/layout/MainLayout.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Menu, { menuWidth } from 'material/components/layout/Menu';
+import Header from 'material/components/layout/Header';
+import Footer from 'material/components/layout/Footer';
+import Paper from '@material-ui/core/Paper';
+import Breadcrumbs from 'material/components/layout/Breadcrumbs';
+import TelegramGroupPromo from 'material/components/misc/TelegramGroupPromo';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  alert: {
+    marginBottom: theme.spacing(3),
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    display: 'inline-flex',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyItems: 'center',
+    paddingLeft: menuWidth,
+    marginTop: theme.spacing(3),
+    height: '100%',
+  },
+  paperContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(3),
+    gap: theme.spacing(3),
+    padding: theme.spacing(3),
+    height: '100%',
+  },
+}));
+
+function MainLayout({ Content }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      <Menu />
+      <main className={classes.content}>
+        <TelegramGroupPromo />
+        {/* Content container */}
+        <Paper elevation={0} className={classes.paperContent}>
+          <Breadcrumbs items={['asd', 'lol']} />
+          <Content />
+        </Paper>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MainLayout;
