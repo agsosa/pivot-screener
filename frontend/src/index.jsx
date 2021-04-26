10683768e26e204b0a1432c2a3995a1d5a72f11d
aleegs <aleej.gs@gmail.com>
2021-04-25 22:16:58 -0300

    Add CssBaseline

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,14 @@
+// TODO: Add babel https://material-ui.com/es/guides/minimizing-bundle-size/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, rootStore } from 'models/Root';
 import App from 'material/lib/App';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <Provider value={rootStore}>
       <App />
     </Provider>
