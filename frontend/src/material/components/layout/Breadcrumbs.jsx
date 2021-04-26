b82d9e1c8e61f1b239e84fdf6d343d67b13b1833
aleegs <aleej.gs@gmail.com>
2021-04-26 01:47:44 -0300

    Create Breadcrumbs component

---
+++ frontend/src/material/components/layout/Breadcrumbs.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import MaterialBreadcrumbs from '@material-ui/core/Breadcrumbs';
+import { PropTypes } from 'prop-types';
+
+function Breadcrumbs({ items }) {
+  const validItems = Array.isArray(items) && items.length > 0;
+
+  return (
+    <MaterialBreadcrumbs aria-label='breadcrumb'>
+      {validItems ? (
+        items.map((q, i) => {
+          let color = 'inherit';
+          if (i === items.length - 1) color = 'textPrimary';
+
+          return (
+            <Typography key={q} color={color}>
+              {q}
+            </Typography>
+          );
+        })
+      ) : (
+        <Typography color='inherit'>Pivot Screener</Typography>
+      )}
+    </MaterialBreadcrumbs>
+  );
+}
+
+Breadcrumbs.defaultProps = {
+  items: null,
+};
+
+Breadcrumbs.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default Breadcrumbs;
