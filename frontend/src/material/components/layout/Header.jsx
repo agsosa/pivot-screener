31768bdd646511ed492de71d7f59d14dcc7d09e5
aleegs <aleej.gs@gmail.com>
2021-04-26 01:46:42 -0300

    Create responsive Header

--- frontend/src/material/components/layout/Header.jsx
+++ frontend/src/material/components/layout/Header.jsx
@@ -1,7 +1,63 @@
-import * as React from 'react';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import TradingViewPrices from 'material/components/misc/TradingViewPrices';
+import HeaderBG from 'img/headerbg.png';
 
-function Header() {
-  return null;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    gap: theme.spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  appBar: {
+    padding: '5px',
+    backgroundImage: `url(${HeaderBG})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  },
+  menuButton: {
+    position: 'absolute',
+    left: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  },
+  title: { [theme.breakpoints.up('sm')]: { marginLeft: '10px' } },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {/* App bar */}
+      <AppBar position='static' className={classes.appBar}>
+        {/* TV prices widget */}
+        <TradingViewPrices responsive />
+        <Toolbar className={classes.toolbar}>
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h5' className={classes.title}>
+            Pivot Screener
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Header;
