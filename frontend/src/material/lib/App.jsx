bb95805baad740c7889c7828349d2a63da9f0c81
aleegs <aleej.gs@gmail.com>
2021-04-26 00:07:27 -0300

    Add ThemeProvider

--- frontend/src/material/lib/App.jsx
+++ frontend/src/material/lib/App.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorPage from 'material/pages/ErrorPage';
-import Content from 'material/components/layout/Content';
-import Footer from 'material/components/layout/Footer';
-import Header from 'material/components/layout/Header';
+import MainLayout from 'material/components/layout/MainLayout';
 import Routes from 'material/lib/Routes';
+import CustomTheme from 'material/lib/CustomTheme';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 export default function App() {
+  function AppContent() {
+    return (
+      <Switch>
+        {Routes.map((route) => (
+          <Route exact path={route.path} component={route.component} key={route.path} />
+        ))}
+        <Route component={ErrorPage} />
+      </Switch>
+    );
+  }
+
   return (
     <Router>
-      <Header />
-      <Content>
-        <Switch>
-          {Routes.map((route) => (
-            <Route exact path={route.path} component={route.component} key={route.path} />
-          ))}
-          <Route component={ErrorPage} />
-        </Switch>
-      </Content>
-      <Footer />
+      <ThemeProvider theme={CustomTheme}>
+        <MainLayout Content={AppContent} />
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ frontend/src/material/lib/CustomTheme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import pink from '@material-ui/core/colors/pink';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#001529',
+    },
+    secondary: pink,
+  },
+});
