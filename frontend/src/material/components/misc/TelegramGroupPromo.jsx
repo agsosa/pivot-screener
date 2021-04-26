333065d512dfc17513f1afa074339f6824f72d5a
aleegs <aleej.gs@gmail.com>
2021-04-26 00:07:49 -0300

    Create Telegram Group Promo alert component

---
+++ frontend/src/material/components/misc/TelegramGroupPromo.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import InfoIcon from '@material-ui/icons/Info';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    alignSelf: 'center',
+    display: 'inline-flex',
+  },
+  alert: {
+    flex: 1,
+    flexDirection: 'row',
+    display: 'inline-flex',
+  },
+}));
+
+function TelegramGroupPromo() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Alert icon={<InfoIcon />} className={classes.alert} severity='warning'>
+        <b>
+          Camarilla Pivot Trading Group •{' '}
+          <a href='https://t.me/camarillacruisin' target='_blank' rel='noopener noreferrer'>
+            Join on Telegram
+          </a>
+        </b>
+      </Alert>
+    </div>
+  );
+}
+
+export default TelegramGroupPromo;
