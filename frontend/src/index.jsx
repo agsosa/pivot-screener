226a9f747f9a17f1f1664dc90e57fd6ef6e536dd
aleegs <aleej.gs@gmail.com>
2021-04-25 21:24:57 -0300

    Clean up index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import MainLayout from 'components/layout/MainLayout';
 import { Provider, rootStore } from 'models/Root';
 import App from 'material/lib/App';
 
-function App_Old() {
-  return (
-    <Provider value={rootStore}>
-      <div className='App'>
-        <MainLayout />
-      </div>
-    </Provider>
-  );
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider value={rootStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
