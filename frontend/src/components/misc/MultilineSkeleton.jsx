cb12e2c079690493e9504f29552f95ff2bf007b0
aleegs <aleej.gs@gmail.com>
2021-04-28 20:49:46 -0300

    Implement Multiline Skeleton

---
+++ frontend/src/components/misc/MultilineSkeleton.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { PropTypes } from 'prop-types';
+
+function MultilineSkeleton({ lines, ...props }) {
+  return Array.from(Array(lines).keys()).map((q) => <Skeleton key={q} {...props} />);
+}
+
+MultilineSkeleton.defaultProps = {
+  lines: 1,
+};
+
+MultilineSkeleton.propTypes = {
+  lines: PropTypes.number,
+};
+
+export default MultilineSkeleton;
