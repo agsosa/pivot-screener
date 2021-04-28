33211bb70e3d8392382ecc6e7453cae2eda367c0
aleegs <aleej.gs@gmail.com>
2021-04-27 23:54:23 -0300

    Add navLinks

--- frontend/src/material/components/layout/Drawer.jsx
+++ frontend/src/material/components/layout/Drawer.jsx
@@ -18,6 +18,10 @@ import AppsIcon from '@material-ui/icons/Apps';
 import DvrIcon from '@material-ui/icons/Dvr';
 import Typography from '@material-ui/core/Typography';
 import { headerSafePadding } from 'material/components/layout/Header';
+import { useHistory } from 'react-router';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 export const menuWidth = 240;
 
@@ -49,62 +53,144 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+/* 
+  navLinks items:
+    {
+      icon: Component to render between ListItemIcon (optional)
+      text: String to display as title
+      description: String to display as subtitle
+      disabled: Boolean to indicate if it's disabled (optional)
+      renderDividerBelow: Boolean to render a <Divider/> below this item (optional)
+
+      The following props have to be used individually:
+        onClick: Function to execute on click (optional)
+        path: Path to navigate to (optional)
+        children: Array of navLinks to display as children with <Collapse /> (optional)
+    }
+*/
+const camScreenersLinks = [
+  { text: 'Cryptocurrency', path: '/screener/camarilla/cryptocurrency' },
+  { text: 'Forex', path: '/screener/camarilla/forex' },
+  { text: 'Commodities', path: '/screener/camarilla/commodities' },
+  { text: 'Indices', path: '/screener/camarilla/indices' },
+  { text: 'Stocks', path: '/screener/camarilla/stocks', disabled: true },
+];
+
+const cprScreenersLinks = [
+  { text: 'Cryptocurrency', path: '/screener/cpr/cryptocurrency' },
+  { text: 'Forex', path: '/screener/cpr/forex' },
+  { text: 'Commodities', path: '/screener/cpr/commodities' },
+  { text: 'Indices', path: '/screener/cpr/indices' },
+  { text: 'Stocks', path: '/screener/cpr/stocks', disabled: true },
+];
+
+const navLinks = [
+  { icon: <HomeIcon />, text: 'Home (Chart)', path: '/' },
+  { icon: <TocIcon />, text: 'CPR Screeners', children: cprScreenersLinks },
+  {
+    icon: <ShowChartIcon />,
+    text: 'CAM Screeners',
+    children: camScreenersLinks,
+    renderDividerBelow: true,
+  },
+  { icon: <DvrIcon />, text: 'Live Feed', path: '/', disabled: true },
+  { icon: <BuildIcon />, text: 'Backtest', path: '/', disabled: true },
+  { icon: <AppsIcon />, text: 'Calculator', path: '/calculator', renderDividerBelow: true },
+  { icon: <TelegramIcon />, text: 'Contact (Telegram)', onClick: () => window.open('https://t.me/xref1x', '_blank') },
+  { icon: <FavoriteIcon />, text: 'Donate Crypto', onClick: () => console.log('hi') },
+];
+
 function Drawer() {
+  const history = useHistory();
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [openChildren, setOpenChildren] = React.useState([]); // Array of strings to know which navLinks with children are open
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
+  function handleNavLinkClick(navLink) {
+    // Handle navLink.path
+    if (navLink.path) {
+      history.push(navLink.path);
+    }
+
+    // Handle navLink.onClick
+    if (navLink.onClick && typeof navLink.onClick === 'function') navLink.onClick();
+
+    // Handle navLink.children (open/close children list)
+    if (navLink.children && Array.isArray(navLink.children))
+      // Toggle (remember than openChildren is an array of strings containing navLink.text if this navLink is open)
+      setOpenChildren((old) => {
+        if (openChildren.find((q) => q === navLink.text)) return old.filter((q) => q !== navLink.text);
+        else return [...old, navLink.text];
+      });
+  }
+
+  function RenderNavLink({ navLink, isChildren, hasChildren, isCollapseOpen }) {
+    console.log(navLink.text, isChildren);
+    return (
+      <ListItem
+        button
+        onClick={() => handleNavLinkClick(navLink)}
+        disabled={navLink.disabled}
+        style={isChildren ? { paddingLeft: '8%' } : {}}>
+        <ListItemIcon>{navLink.icon}</ListItemIcon>
+        <ListItemText primary={navLink.text} secondary={navLink.description} />
+        {/* Render expand icon if this navLink has children */}
+        {
+          (() => {
+            // Inline func for this nested condition
+            if (hasChildren) {
+              if (isCollapseOpen) return <ExpandLess />;
+              else return <ExpandMore />;
+            }
+          })() /* call the inline func */
+        }
+      </ListItem>
+    );
+  }
+
   const drawer = (
-    <div>
-      <div className={classes.toolbar} />
+    <>
+      <div className={classes.toolbar} /> {/* Div to position the drawer below the top appbar */}
+      {/* Drawer header */}
       <Hidden smDown>
         <Typography variant='h5' className={classes.title}>
           Pivot Screener
         </Typography>
       </Hidden>
+      {/* Drawer links */}
       <List>
-        <ListItem button onClick={() => console.log('home')}>
-          <ListItemIcon>{<HomeIcon />}</ListItemIcon>
-          <ListItemText primary='Home (Chart)' />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>{<TocIcon />}</ListItemIcon>
-          <ListItemText primary='CPR Screeners' />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>{<ShowChartIcon />}</ListItemIcon>
-          <ListItemText primary='Camarilla Screeners' />
-        </ListItem>
+        {navLinks.map((navLink) => {
+          const hasChildren = navLink.children && Array.isArray(navLink.children);
+          const isCollapseOpen = openChildren.find((q) => q === navLink.text);
+
+          return (
+            <div key={navLink.text}>
+              {/* Render nav link */}
+              <RenderNavLink navLink={navLink} hasChildren={hasChildren} isCollapseOpen={isCollapseOpen} />
+
+              {/* Render children array */}
+              {hasChildren && (
+                <Collapse in={isCollapseOpen} timeout='auto' unmountOnExit>
+                  <List component='div' disablePadding>
+                    {navLink.children.map((child) => (
+                      <RenderNavLink key={child.text} navLink={child} isChildren />
+                    ))}
+                  </List>
+                </Collapse>
+              )}
+
+              {/* Render divider */}
+              {navLink.renderDividerBelow && <Divider />}
+            </div>
+          );
+        })}
       </List>
-      <Divider />
-      <ListItem button disabled>
-        <ListItemIcon>{<DvrIcon />}</ListItemIcon>
-        <ListItemText primary='Live Feed' />
-      </ListItem>
-      <ListItem button disabled>
-        <ListItemIcon>{<BuildIcon />}</ListItemIcon>
-        <ListItemText primary='Backtest' />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>{<AppsIcon />}</ListItemIcon>
-        <ListItemText primary='Calculator' />
-      </ListItem>
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon>{<TelegramIcon />}</ListItemIcon>
-          <ListItemText primary='Contact (Telegram)' />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>{<FavoriteIcon />}</ListItemIcon>
-          <ListItemText primary='Donate Crypto' />
-        </ListItem>
-      </List>
-    </div>
+    </>
   );
 
   return (
