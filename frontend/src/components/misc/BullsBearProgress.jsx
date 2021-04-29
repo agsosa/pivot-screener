9dba6e5a405f7ae36f369bcc92ed2e7d3c0e2e7b
aleegs <aleej.gs@gmail.com>
2021-04-28 22:18:09 -0300

    Add text

--- frontend/src/components/misc/BullsBearProgress.jsx
+++ frontend/src/components/misc/BullsBearProgress.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  progressTextContainer: {
+    marginBottom: theme.spacing(1),
+  },
+}));
 
 const BorderLinearProgress = withStyles(() => ({
   root: {
@@ -17,12 +24,35 @@ const BorderLinearProgress = withStyles(() => ({
   },
 }))(LinearProgress);
 
-function BullsBearProgress({ value, ...props }) {
-  return <BorderLinearProgress value={value} variant='determinate' {...props} />;
+function BullsBearProgress({ bullsPercent, bearsPercent, ...props }) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.progressTextContainer}>
+        🐂{' '}
+        <Typography variant='inherit' style={{ color: 'green' }}>
+          Bulls {bullsPercent.toFixed(1)}%
+        </Typography>{' '}
+        <b>vs</b>{' '}
+        <Typography variant='inherit' style={{ color: 'red' }}>
+          {bearsPercent.toFixed(1)}% Bears
+        </Typography>{' '}
+        🐻
+      </div>
+      <BorderLinearProgress value={bullsPercent} variant='determinate' {...props} />
+    </div>
+  );
 }
 
+BullsBearProgress.defaultProps = {
+  bullsPercent: 0,
+  bearsPercent: 0,
+};
+
 BullsBearProgress.propTypes = {
-  value: PropTypes.number.isRequired,
+  bullsPercent: PropTypes.number,
+  bearsPercent: PropTypes.number,
 };
 
 export default BullsBearProgress;
