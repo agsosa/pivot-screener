37e9aaa048c7c85e5277482a1262abaf6ddfa5e4
aleegs <aleej.gs@gmail.com>
2021-04-28 22:19:06 -0300

    Refactor CPRStats to use Material UI

--- frontend/src/components/misc/CPRStats.jsx
+++ frontend/src/components/misc/CPRStats.jsx
@@ -1,119 +1,57 @@
 import React from 'react';
-import { Card, Statistic, Row, Col, Progress, Skeleton, Button } from 'antd';
-import {
-  FallOutlined,
-  RiseOutlined,
-  ExpandOutlined,
-  VerticalAlignMiddleOutlined,
-  PauseOutlined,
-} from '@ant-design/icons';
 import { observer } from 'mobx-react-lite';
 import { PropTypes } from 'prop-types';
 import { useMst } from 'models/Root';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import TrendingDownIcon from '@material-ui/icons/TrendingDown';
+import PauseIcon from '@material-ui/icons/Pause';
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import VerticalAlignCenterIcon from '@material-ui/icons/VerticalAlignCenter';
+import BullsBearProgress from './BullsBearProgress';
+import { StatsCardItem, StatsCardColumn, StatsCardContainer } from './StatsCard';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+  },
+}));
 
 const CPRStats = ({ timeframe, futureMode }) => {
-  const { tickers, cprStats, toggleCPRStatsPanel, cprStatsPanelVisible } = useMst((store) => ({
-    tickers: store.tickers,
+  const classes = useStyles();
+
+  const { cprStats, toggleCPRStatsPanel, cprStatsPanelVisible } = useMst((store) => ({
     cprStats: store.cprStats,
     toggleCPRStatsPanel: store.toggleCPRStatsPanel,
     cprStatsPanelVisible: store.cprStatsPanelVisible,
   }));
 
-  if (!tickers || (tickers.length === 0 && cprStatsPanelVisible)) return <Skeleton />;
-
   const stats = cprStats(timeframe, futureMode);
 
   return (
-    <div>
-      <Button className='btn' onClick={toggleCPRStatsPanel}>
+    <div className={classes.root}>
+      <Button variant='outlined' color='primary' onClick={toggleCPRStatsPanel}>
         {cprStatsPanelVisible ? 'Hide Statistics' : 'Show Statistics'}
       </Button>
-
       {cprStatsPanelVisible ? (
-        <div>
-          <div className='site-statistic-demo-card'>
-            <Row gutter={12}>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='Untested CPR'
-                    value={stats.untestedCount}
-                    precision={0}
-                    valueStyle={{ color: 'black' }}
-                    prefix='🧲'
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='Neutral'
-                    value={stats.neutralCount}
-                    precision={0}
-                    valueStyle={{ color: 'gray' }}
-                    prefix={<PauseOutlined />}
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='Above CPR'
-                    value={stats.aboveCount}
-                    precision={0}
-                    valueStyle={{ color: '#3f8600' }}
-                    prefix={<RiseOutlined />}
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='Below CPR'
-                    value={stats.belowCount}
-                    precision={0}
-                    valueStyle={{ color: '#cf1322' }}
-                    prefix={<FallOutlined />}
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='CPR Width < 1%'
-                    value={stats.tightCount}
-                    precision={0}
-                    valueStyle={{ color: '#df4294' }}
-                    prefix={<VerticalAlignMiddleOutlined />}
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title='CPR Width > 1%'
-                    value={stats.wideCount}
-                    precision={0}
-                    valueStyle={{ color: '#2196F3' }}
-                    prefix={<ExpandOutlined />}
-                    suffix=''
-                  />
-                </Card>
-              </Col>
-            </Row>
-          </div>
+        <>
+          <StatsCardContainer>
+            <StatsCardColumn>
+              <StatsCardItem label='Untested CPR' count={stats.untestedCount} prefix='🧲' />
+              <StatsCardItem label='Above CPR' count={30} prefix={<TrendingUpIcon />} />
+              <StatsCardItem label='CPR Width < 1%' count={30} prefix={<VerticalAlignCenterIcon />} />
+            </StatsCardColumn>
+            <StatsCardColumn>
+              <StatsCardItem label='Neutral' count={30} prefix={<PauseIcon />} />
+              <StatsCardItem label='Below CPR' count={30} prefix={<TrendingDownIcon />} />
+              <StatsCardItem label='CPR Width > 1%' count={30} prefix={<FullscreenIcon />} />
+            </StatsCardColumn>
+          </StatsCardContainer>
 
-          <div className='progress-bar-container'>
-            🐂 <font color='green'>Bulls {stats.bullsPercent.toFixed(1)}%</font> <b>vs</b>{' '}
-            <font color='red'>{stats.bearsPercent.toFixed(1)}% Bears</font> 🐻
-            <Progress percent={100} success={{ percent: stats.bullsPercent }} showInfo={false} strokeColor='red' />
-          </div>
-        </div>
+          <BullsBearProgress bullsPercent={stats.bullsPercent} bearsPercent={stats.bearsPercent} />
+        </>
       ) : null}
     </div>
   );
